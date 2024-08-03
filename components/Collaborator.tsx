8acbcf9d663040dfe61b699a6b0e76db330492f0
Danny <dannydotdev@gmail.com>
2024-08-03 14:47:59 +0100

    Viewers and editors responsibilities

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React, { useState } from "react";
+import UserTypeSelector from "./UserTypeSelector";
 
 const Collaborator = ({
   creatorId,
@@ -12,7 +14,42 @@ const Collaborator = ({
   const ShareDocumentHandler = async (type: string) => {};
   const removeCollaboratorHandler = async (email: string) => {};
 
-  return <div>Collaborator</div>;
+  return (
+    <li className="flex items-center justify-between gap-2 py-3">
+      <div className="flex gap-2">
+        <Image
+          src={collaborator.avatar}
+          alt={collaborator.name}
+          width={36}
+          height={36}
+          className="size-9  rounded-full"
+        />
+        <div>
+          <p className="line-clamp-1 text-sm font-semibold leading-4 text-white">
+            {collaborator.name}
+            <span className="text-10-regular pl-2 text-blue-100">
+              {loading && "updating..."}
+            </span>
+          </p>
+          <p className="text-sm font-light text-blue-100">
+            {collaborator.email}
+          </p>
+        </div>
+      </div>
+
+      {creatorId === collaborator.id ? (
+        <p className="text-sm  text-bluwe-100">Owner</p>
+      ) : (
+        <div className="flex  items-center">
+          <UserTypeSelector
+            userType={userType as UserType}
+            setUserType={setUserType || "viewer"}
+            onClickHandler={ShareDocumentHandler}
+          />
+        </div>
+      )}
+    </li>
+  );
 };
 
 export default Collaborator;
